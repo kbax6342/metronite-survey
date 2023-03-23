1689975097fbdf9b5bb5e2cfd96ca12069a1b9c7
kbax6342 <k3vin.baxt3r@gmail.com>
2023-03-23 01:32:40 -0400

    added database connection

--- page4_form.php
+++ page4_form.php
@@ -42,17 +42,21 @@
 // $password = "root";
 
 
-// try
-// {
-//     $conn = new PDO("mysql:host=$host;dbname=survey", $username, $password);
+    
+    // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    // $cleardb_server = $cleardb_url["host"];
+    // $cleardb_username = $cleardb_url["user"];
+    // $cleardb_password = $cleardb_url["pass"];
+    // $cleardb_db = substr($cleardb_url["path"],1);
+    // $active_group = 'default';
+    // $query_builder = TRUE;
 
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// //     echo "Connected successfully";
-//  }
-// catch(PDOException $e)
-// {
-//     echo "Connection failed: " . $e->getMessage();
-// }
+    // // Connect to DB
+    // $conn = new PDO("mysql:host=$cleardb_server;dbname=$cleardb_db", $cleardb_username, $cleardb_password, );
+    // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
 
  if (isset($_POST['states'])) {
  if (!empty($_SESSION['post'])){
@@ -90,6 +94,7 @@
  $conn->query($sql);
 
 $sql = "SELECT user_id, names, email FROM mpage";
+
 $result = $conn->query($sql);
  
 
